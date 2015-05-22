c4601b69cc1335b0534c9341d1719dfee1575d55
jkneal <jkneal@indiana.edu>
2015-05-22 15:04:40 -0400

    Init solution for ex15

--- src/main/java/edu/train/product/InventoryController.java
+++ src/main/java/edu/train/product/InventoryController.java
@@ -3,8 +3,10 @@ package edu.train.product;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/inventory")
@@ -25,6 +27,23 @@ public class InventoryController {
   @RequestMapping(value="/delete", method=RequestMethod.POST)
   public String deleteProduct(HttpServletRequest request) {
     request.setAttribute("method", "Delete Product");
+    return "test-mappings";
+  }
+  
+  @RequestMapping(value="/search", method=RequestMethod.GET)
+  public String searchProducts(@RequestParam("searchTerm") String searchTerm,
+                               @RequestParam(value="sortBy", defaultValue="name") String sortBy,
+                               HttpServletRequest request) {
+    request.setAttribute("searchTerm", searchTerm);
+    request.setAttribute("sortBy", sortBy);
+    request.setAttribute("method", "Search");
+    return "test-mappings";
+  }
+  
+  @RequestMapping(value="/delete/{id}", method=RequestMethod.POST)
+  public String deleteProductById(@PathVariable String id, HttpServletRequest request) {
+    request.setAttribute("id", id);
+    request.setAttribute("method", "Delete Product By Id");
     return "test-mappings";
   }
 
