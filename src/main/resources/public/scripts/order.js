770524563768c7393ee88c402d32e2ac4a1e1fd9
jkneal <jkneal@indiana.edu>
2015-05-26 20:02:53 -0400

    Init solution for ex25

---
+++ src/main/resources/public/scripts/order.js
@@ -0,0 +1,42 @@
+
+function newOrder() {
+  var orderDetails = [];
+  
+  return {
+	id: Math.random(),
+	submitted: false,
+	add: function(product, quantity) {
+	  quantity = quantity || 1;
+	  orderDetails.push({product: product, quantity: quantity});
+	},
+	updateQuantity: function(product, quantity) {
+	  orderDetails.forEach(function (orderDetail) {
+		if (orderDetail.product.id === product.id) {
+			orderDetail.quantity = quantity;
+		}
+	  })	
+	},
+	getTotal: function() {
+	  var total = 0;
+	  orderDetails.forEach(function (orderDetail) {
+		total += orderDetail.product.price * orderDetail.quantity; 
+	  })
+	  return total;
+	},
+	complete: function(successCallback) {
+	  if (this.submitted) {
+		throw Error('Order has already been submitted');  
+	  }	
+	  $.ajax({url: '/orders',
+	    type: 'POST',
+		data: {id: this.id, details: JSON.stringify(orderDetails)}, 
+	    contentType: 'application/json'}).done(function(response) {
+	      successCallback(response);  
+	    }).fail(function() {
+	      alert('Error in request');
+	    });	
+	  this.submitted = true;
+	}
+  }
+}
+  
