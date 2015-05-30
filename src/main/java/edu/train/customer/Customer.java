78dc9922fd9695c83b5dfca39469cb6cc492c934
James Bennett <jwbennet@gmail.com>
2015-05-30 14:21:46 -0400

    Added solution for exercise 4

--- src/main/java/edu/train/customer/Customer.java
+++ src/main/java/edu/train/customer/Customer.java
@@ -1,16 +1,26 @@
 package edu.train.customer;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 
+@Entity
+@Table(name="CUSTOMER_T")
 @Data
 public class Customer {
 
 	@Id
+	@Column(name="id")
 	private int id;
 
+	@Column(name="username")
 	private String username;
 	
-	
+	@Embedded
+	private Name name;
+
 }
