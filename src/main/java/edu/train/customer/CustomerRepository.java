1716323ea3bce73aee36881228c879a66362f826
James Bennett <jwbennet@gmail.com>
2015-05-30 14:05:55 -0400

    Added baseline classes for exercise 4

---
+++ src/main/java/edu/train/customer/CustomerRepository.java
@@ -0,0 +1,9 @@
+package edu.train.customer;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "customers", path = "customers")
+public interface CustomerRepository extends PagingAndSortingRepository<Customer, Integer> {
+
+}
