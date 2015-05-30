370326bd0cc86f93936a7ee1aa5dd358dd5f58fc
jkneal <jkneal@indiana.edu>
2015-05-30 12:39:57 -0400

    Init sol for ex34

---
+++ src/main/resources/public/angular/scripts/app.js
@@ -0,0 +1,33 @@
+angular.module('storeApp', ['ngRoute']);
+
+angular.module('storeApp').config(function ($routeProvider) {
+    $routeProvider .when('/home', {
+        templateUrl: 'partials/home.html'
+    }).when('/add', {
+        templateUrl: 'partials/add-product.html'
+    }).when('/checkout', {
+        templateUrl: 'partials/checkout.html'
+    }).otherwise({
+        redirectTo: '/home'
+    });
+});
+
+angular.module('storeApp').filter('categories', function () {
+        return function (items) {
+            var categories = [];
+            var keys = {};
+
+            for (var i = 0; i < items.length; i++) {
+                var val = items[i].category;
+                if (angular.isUndefined(keys[val])) {
+                    keys[val] = true;
+                    categories.push(val);
+                }
+            }
+
+            return categories;
+        };
+    }
+);
+
+angular.module('storeApp').constant('ProductServiceUrl', 'http://localhost:8080/inventory');
---
+++ src/main/resources/public/angular/scripts/controllers/store-controller.js
@@ -0,0 +1,36 @@
+angular.module('storeApp').controller('storeCrtl', function ($scope, ProductService, $rootScope) {
+    $scope.name = 'Joe\'s Sports Store';
+    $scope.address = '5100 51st Chicago, IL';
+    $scope.status = 'Open';
+
+    $scope.getStatus = function () {
+        return $scope.status;
+    }
+
+    $scope.products = [];
+    ProductService.getAllProducts().then(function (result) {
+        $scope.products = result.data;
+    });
+
+    $scope.addProduct = function (productName, productDescription, productCategory, productPrice) {
+        var newProduct = {
+            name: productName,
+            description: productDescription,
+            category: productCategory,
+            price: productPrice
+        };
+
+        ProductService.addProduct(newProduct).then(function (result) {
+            $scope.products = result.data;
+            $scope.productAdded = true;
+        });
+    };
+
+    $scope.productAdded = false;
+
+    $scope.removeProduct = function (index) {
+        ProductService.removeProduct(index).then(function (result) {
+            $scope.products = result.data;
+        });
+    };
+});
