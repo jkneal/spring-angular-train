32a3ef2a4c7847302e2e90e70c8f35c78447095e
James Bennett <jwbennet@gmail.com>
2015-05-30 15:27:11 -0400

    Added base files for exercise 5

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO STORE_T (NAME, OWNER, OPEN, WEBSITE) VALUES ('Joe''s Sports Store', 'Joe', 'OPEN', 'joesports.com');
+INSERT INTO STORE_T (NAME, OWNER, OPEN, WEBSITE, CREATE_DT) VALUES ('Joe''s Sports Store', 'Joe', 'OPEN', 'joesports.com', parsedatetime('01-09-2012 00:00:00.000', 'dd-MM-yyyy hh:mm:ss.SS'));
 
 INSERT INTO ORDER_T (ID, TOTAL, CREATE_DT, VERSION) VALUES(ORDER_SEQ.nextval, 19.99, parsedatetime('17-09-2012 18:47:52.690', 'dd-MM-yyyy hh:mm:ss.SS'), 1);
 
-INSERT INTO PRODUCT_T (NAME, QTY, BACKORDER_AVAILABLE) VALUES('Basketball', 5, 'Y');
+INSERT INTO PRODUCT_T (NAME, QTY, BACKORDER_AVAILABLE, CREATE_DT) VALUES('Basketball', 5, 'Y', current_timestamp());
 
-INSERT INTO CUSTOMER_T (USERNAME, FIRST_NAME, LAST_NAME) VALUES ('johndoe', 'John', 'Doe');
-INSERT INTO CUSTOMER_T (USERNAME, FIRST_NAME, LAST_NAME) VALUES ('janedoe', 'Jane', 'Doe');
+INSERT INTO CUSTOMER_T (USERNAME, FIRST_NAME, LAST_NAME, CREATE_DT) VALUES ('johndoe', 'John', 'Doe', current_timestamp());
+INSERT INTO CUSTOMER_T (USERNAME, FIRST_NAME, LAST_NAME, CREATE_DT) VALUES ('janedoe', 'Jane', 'Doe', current_timestamp());
