f8ef83f14fd0a73ca6731225d0adeb21cdb66bda
jkneal <jkneal@indiana.edu>
2015-05-30 12:04:18 -0400

    Update starting point for ex32

--- src/main/java/edu/train/product/InventoryController.java
+++ src/main/java/edu/train/product/InventoryController.java
@@ -1,5 +1,9 @@
 package edu.train.product;
 
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/inventory")
 public class InventoryController {
+  protected final Log logger = LogFactory.getLog(getClass());
   
   private ProductRepository productRepository;
   
@@ -32,13 +37,12 @@ public class InventoryController {
     return productRepository.findByNameMatching(searchTerm, new Sort(sortBy));
   }
   
-  @RequestMapping(value="/{id}", method=RequestMethod.DELETE)
-  public void deleteProductById(@PathVariable Integer id) {
-    Product product = productRepository.findOne(id);
-    if (product == null) {
-      throw new ProductNotFoundException();
-    }
-    productRepository.delete(id);
+  @RequestMapping(value="/{index}", method=RequestMethod.DELETE)
+  public Iterable<Product> deleteProductByIndex(@PathVariable Integer index) {
+    List<Product> products = (List<Product>) productRepository.findAll();
+    Product productToDelete = products.remove(index.intValue());
+    productRepository.delete(productToDelete);
+    return products;
   }
   
   @Autowired
