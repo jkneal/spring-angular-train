c39cc26f2fc82454be370cfd78b2e7cac201a9d0
James Bennett <jwbennet@gmail.com>
2015-05-31 23:43:29 -0400

    Added solution for exercise 6

--- src/main/java/edu/train/order/Order.java
+++ src/main/java/edu/train/order/Order.java
@@ -1,12 +1,17 @@
 package edu.train.order;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.persistence.Transient;
@@ -15,6 +20,7 @@ import javax.persistence.Version;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import edu.train.GenericEntity;
+import edu.train.customer.Customer;
 
 @Entity
 @Table(name="ORDER_T")
@@ -35,5 +41,12 @@ public class Order extends GenericEntity {
   
   @Version
   private int version;
+  
+  @ManyToOne(fetch=FetchType.LAZY)
+  @JoinColumn(name="CUSTOMER_ID", referencedColumnName="ID")
+  private Customer customer;
+  
+  @OneToMany(mappedBy="order")
+  private List<OrderItem> orderItems;
 
 }
--- src/main/java/edu/train/order/OrderItem.java
+++ src/main/java/edu/train/order/OrderItem.java
@@ -3,11 +3,14 @@ package edu.train.order;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import edu.train.GenericEntity;
+import edu.train.product.Product;
 
 @Entity
 @Table(name="ORDER_ITEM_T")
@@ -20,5 +23,12 @@ public class OrderItem extends GenericEntity {
 	
 	@Column(name = "QTY")
 	private int quantity;
+
+	@ManyToOne
+	@JoinColumn(name="ORDER_ID", referencedColumnName="ID")
+	private Order order;
 	
+	@ManyToOne
+	@JoinColumn(name="PRODUCT_ID", referencedColumnName="ID")
+	private Product product;
 }
