53172114ae7d140a242c769fff0374a4b818f1e0
James Bennett <jwbennet@gmail.com>
2015-06-01 23:21:09 -0400

    Added solution for exercise 9

--- src/main/java/edu/train/store/Store.java
+++ src/main/java/edu/train/store/Store.java
@@ -8,9 +8,14 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import org.hibernate.validator.constraints.NotBlank;
+
 import edu.train.GenericEntity;
 import edu.train.address.Address;
 
@@ -26,17 +31,25 @@ public class Store extends GenericEntity {
   @GeneratedValue
   private Integer id;
   
+  @NotBlank(message = "The name must not be blank")
+  @Size(min = 5, max = 40, message = "The store name must be between 5 and 40 characters in length")
   private String name;
   
+  @NotBlank(message = "The owner must not be blank")
+  @Size(min = 1, max = 40, message = "The store owner must be between 1 and 40 characters in length")
   private String owner;
   
   @Column(name="WEBSITE")
+  @NotBlank(message = "The owner must not be blank")
+  @Size(min = 1, max = 120, message = "The store owner must be between 1 and 120 characters in length")
   private String website;
   
   @Enumerated(EnumType.STRING)
+  @NotNull(message = "The open status must not be null")
   private StoreStatus open;
   
   @Embedded
+  @NotNull(message = "The store address must not be null")
   private Address address;
 
 }
