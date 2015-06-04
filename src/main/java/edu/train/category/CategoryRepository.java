6f6aee75e34e40ee96480f385f201e68641ad9f3
jkneal <jkneal@indiana.edu>
2015-06-04 18:37:27 -0400

    Update data objects 18 Access exception 23 Add search method to product repo 23 Access error page 22 User Session

---
+++ src/main/java/edu/train/category/CategoryRepository.java
@@ -0,0 +1,9 @@
+package edu.train.category;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "categories", path = "categories")
+public interface CategoryRepository extends PagingAndSortingRepository<Category, Integer> {
+
+}
