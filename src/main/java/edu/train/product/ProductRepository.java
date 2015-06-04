6e8f50eba4209df3f47e9a4b35a2203589262e87
jkneal <jkneal@indiana.edu>
2015-06-04 18:31:30 -0400

    Access exception 23 Add search method to product repo 23

--- src/main/java/edu/train/product/ProductRepository.java
+++ src/main/java/edu/train/product/ProductRepository.java
@@ -1,9 +1,15 @@
 package edu.train.product;
 
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "products", path = "products")
 public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
 
+  @Query("select p from Product p where p.name like %?1%")
+  List<Product> findByNameMatching(String name, Sort sort);
 }
