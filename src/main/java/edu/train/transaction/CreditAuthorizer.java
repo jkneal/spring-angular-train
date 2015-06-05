6c958272a3abe5435aba51bee59ddfc5247c6189
jkneal <jkneal@indiana.edu>
2015-06-05 16:33:33 -0700

    Updates from JPA

--- src/main/java/edu/train/transaction/CreditAuthorizer.java
+++ src/main/java/edu/train/transaction/CreditAuthorizer.java
@@ -1,14 +1,15 @@
 package edu.train.transaction;
 
 import lombok.Data;
+import edu.train.name.Name;
 
 @Data
 public class CreditAuthorizer {
   
-  private String name;
+  private Name name;
   private String authorizeEndpoint;
   
-  public CreditAuthorizer(String name, String authorizeEndpoint) {
+  public CreditAuthorizer(Name name, String authorizeEndpoint) {
     super();
     this.name = name;
     this.authorizeEndpoint = authorizeEndpoint;
