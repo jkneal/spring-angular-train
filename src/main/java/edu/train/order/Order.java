f884b5f535a52820c887de9ef6f5d4050f1c7077
jkneal <jkneal@indiana.edu>
2015-06-06 14:56:10 -0700

    Updates from JPA

--- src/main/java/edu/train/order/Order.java
+++ src/main/java/edu/train/order/Order.java
@@ -1,10 +1,10 @@
 package edu.train.order;
 
 import java.math.BigDecimal;
-import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -12,19 +12,20 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import javax.persistence.Transient;
 import javax.persistence.Version;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import edu.train.GenericEntity;
 import edu.train.customer.Customer;
 
 @Entity
 @Table(name="ORDER_T")
 @SequenceGenerator(name="ORDER_SEQ_GEN", sequenceName="ORDER_SEQ")
 @Data
-public class Order {
+@EqualsAndHashCode(callSuper=true)
+public class Order extends GenericEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="ORDER_SEQ_GEN")
@@ -33,18 +34,14 @@ public class Order {
   @Column(name="TOTAL", scale=2, precision=19)
   private BigDecimal total;
   
-  @Temporal(TemporalType.TIMESTAMP)
-  @Column(name="CREATE_DT")
-  private Date createDate;
-  
   @Transient
   private int quantity;
   
   @Version
   private int version;
   
-  @ManyToOne
-  @JoinColumn(name="CUSTOMER_FK")
+  @ManyToOne(fetch=FetchType.LAZY)
+  @JoinColumn(name="CUSTOMER_FK", referencedColumnName="ID")
   private Customer customer;
 
 }
