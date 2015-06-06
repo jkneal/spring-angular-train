b5cf8abd26ab441a0bb2fb7f1f05522e138d2c02
jkneal <jkneal@indiana.edu>
2015-06-06 10:12:07 -0700

    Updates from JPA

---
+++ src/main/java/edu/train/BooleanToStringConverter.java
@@ -0,0 +1,20 @@
+package edu.train;
+
+import javax.persistence.AttributeConverter;
+
+public class BooleanToStringConverter implements AttributeConverter<Boolean, String> {
+	
+	private static final String TRUE_VALUE = "Y";
+	private static final String FALSE_VALUE = "N";
+
+	@Override
+	public String convertToDatabaseColumn(Boolean attribute) {
+		return attribute ? TRUE_VALUE : FALSE_VALUE;
+	}
+
+	@Override
+	public Boolean convertToEntityAttribute(String dbData) {
+		return dbData.equals(TRUE_VALUE);
+	}
+
+}
